2cba4e6e21bdc9ed948cce3ad40c48ece8c5d84d
Sinut Wattanarporn <s_w.piepie@outlook.com.au>
2023-09-16 14:41:36 +0700

    feat: FTC-0 init project

---
+++ lib/src/flutter_timer_camera.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FlutterTimerCamera extends StatelessWidget {
+  const FlutterTimerCamera({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
